87a1ace9c9d4d24f7aa7c1a8b4fd076b645041e6
CoderSoumya007 <116835983+CoderSoumya007@users.noreply.github.com>
2023-09-16 23:36:52 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,41 @@
+// const pr = sum(2, 3);
+// pr.then(console.log(5)).catch(function (err) {
+//     console.log(err)
+// })
+
+// function sum(x, y) {
+//     const promise = new Promise(function (resolve, reject) {
+//         if (x + y > 5) {
+//             resolve(console.log("success"))
+//         } else {
+//             reject("sorry")
+//         }
+//     })
+//     return promise
+// }
+// sum("").then(function (sum) {
+//     x = diff(sum);
+//     return x;
+// }).then(function (x) {
+//     console.log(z(x))
+// });
+
+// function sum(x) {
+//     let promise = new Promise(function (resolve, reject) {
+//         if (x == "") {
+//             x += "sssssssssssss";
+//             resolve(x);
+//         } else {
+//             reject("sorry");
+//         }
+//     })
+//     return promise;
+// }
+// function diff(s) {
+//     s += "zzzzzzzzzzzzzzzzz"
+//     return s;
+// }
+
+// function z(s) {
+//     return s + "xxxxxx";
+// }
